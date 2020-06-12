d089222ffaf4e63a584a35863a694791d1497bf5
Eduardo Freitas <edusf8700@gmail.com>
2020-06-12 15:39:01 -0300

    Criado rota de arquivos estáticos

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import path from "path";
+
 import routes from "./routes";
 
 const app = express();
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.use(routes);
 
