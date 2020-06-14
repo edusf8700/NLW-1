ac2ef56dd0c3163134af787aff8729558413f6e4
Eduardo Freitas <edusf8700@gmail.com>
2020-06-13 21:55:30 -0300

    Criado rota de listagem de items e rota de criação de pontos

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,62 @@
 import express from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  res.json({Olá: 'mundo'});
+routes.get("/items", async (req, res) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    }
+  });
+
+  return res.json(serializedItems);
+})
+
+routes.post('/points', async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = req.body;
+
+
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx('points').insert({
+    image: 'https://images.unsplash.com/photo-1554208873-4292cf6c952d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=40',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: Number) => {
+    return {
+      item_id,
+      point_id
+    }
+  });
+
+  await trx('point_items').insert(pointItems);
+
+  await trx.commit();
+
+  return res.json({ success: true });
 })
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,8 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.use(routes);
