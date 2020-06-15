eef06a8942bc153e2ed898228fbe4d806408093d
Eduardo Freitas <edusf8700@gmail.com>
2020-06-14 22:38:58 -0300

    Configurado o cros

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 
 import routes from "./routes";
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
-
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.use(routes);
