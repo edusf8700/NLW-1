1fb271c03d4945145eb9601de74b53a875878d5c
Eduardo Freitas <edusf8700@gmail.com>
2020-06-16 20:56:41 -0300

    Buscando estados e cidades da API do IBGE

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, Marker, TileLayer } from 'react-leaflet'
-
+import axios from 'axios';
 
 import api from '../../services/api';
 import './styles.css';
@@ -14,14 +14,58 @@ interface item {
   image_url: string
 }
 
+interface IBGEUFResponse {
+  sigla: string
+}
+
+interface IBGECityResponse {
+  nome: string
+}
+
 const CreatePoint = () => {
   const [items, setItems] = useState<item[]>([]); 
+
+  const [ufs, setUfs ]= useState<string[]>([]);
+  const [cities, setCities]= useState<string[]>([]);
+
+  const [selectedUf, setSelectedUf]= useState('0');
+  const [selectedCity, setSelectedCity]= useState('0');
+
+  useEffect(() => {
+    axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+      .then(response => {
+        const ufInitials = response.data.map(uf => uf.sigla);
+        setUfs(ufInitials);
+      })
+  },[])
+
+  useEffect(() => {
+    if(selectedUf === '0') {
+      return;
+    }
+
+    axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+      .then(response => {
+        const cityNames = response.data.map(city => city.nome);
+        setCities(cityNames);
+      })
+  },[selectedUf])
 
   useEffect(() => {
     api.get('/items').then(response => {
       setItems(response.data);
     })
   },[]);
+
+  function handleSelectUf (event: ChangeEvent<HTMLSelectElement>) {
+    const uf = event.target.value;
+    setSelectedUf(uf)
+  } 
+
+  function handleSelectCity (event: ChangeEvent<HTMLSelectElement>) {
+    const city = event.target.value;
+    setSelectedUf(city)
+  }
 
   return (
     <div id="page-create-point">
@@ -61,25 +105,41 @@ const CreatePoint = () => {
             <span>Selecione endereço no mapa</span>
           </legend>
 
-          <Map center={[-3.7401007, -38.5706562]} zoom={13}>
+          <Map center={[-3.7395909, -38.6109092]} zoom={13}>
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             />
-            <Marker position={[-3.7401007, -38.5706562]} />
+            <Marker position={[-3.7395909, -38.6109092]} />
           </Map>
 
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">Estado (UF)</label>
-              <select name="uf" id="uf">
+              <select 
+                name="uf" 
+                id="uf" 
+                value={selectedUf} 
+                onChange={handleSelectUf}
+              >
                 <option value="0">Selecione um UF</option>
+                {ufs.map(uf => ( 
+                  <option key={uf} value={uf}>{uf}</option>
+                ))}
               </select>
             </div>
             <div className="field">
               <label htmlFor="city">Cidade</label>
-              <select name="city" id="city">
+              <select 
+                name="city" 
+                id="city"
+                value={selectedCity}
+                onChange={handleSelectCity}
+              >
                 <option value="0">Selecione um cidade</option>
+                {cities.map(city => ( 
+                  <option key={city} value={city}>{city}</option>
+                ))}
               </select>
             </div>
           </div>
