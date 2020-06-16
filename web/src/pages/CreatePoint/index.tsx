7de1c28b83e506b0a7d4de853dac1437d2423db6
Eduardo Freitas <edusf8700@gmail.com>
2020-06-16 16:09:16 -0300

    Buscando itens de coleta da API

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, Marker, TileLayer } from 'react-leaflet'
 
+
+import api from '../../services/api';
 import './styles.css';
 import logo from '../../assets/logo.svg';
 
+interface item {
+  id: number,
+  title: string,
+  image_url: string
+}
+
 const CreatePoint = () => {
+  const [items, setItems] = useState<item[]>([]); 
+
+  useEffect(() => {
+    api.get('/items').then(response => {
+      setItems(response.data);
+    })
+  },[]);
+
   return (
     <div id="page-create-point">
       <header>
@@ -76,30 +92,12 @@ const CreatePoint = () => {
           </legend>
 
           <ul className="items-grid">
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas" />
-              <span>Lâmpadas</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas" />
-              <span>Lâmpadas</span>
-            </li>
-            <li className="selected">
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas" />
-              <span>Lâmpadas</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas" />
-              <span>Lâmpadas</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas" />
-              <span>Lâmpadas</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas" />
-              <span>Lâmpadas</span>
-            </li>
+            {items.map(item => (  
+              <li key={item.id}>
+                <img src={item.image_url} alt={item.title} />
+                <span>{item.title}</span>
+              </li>
+            ))}
           </ul>
 
           <button type="submit">Cadastrar ponto de coleta</button>
